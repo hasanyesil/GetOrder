4ecbe5dde6098c5ceb715af0219b8788fed36b26
Hasan Yesil <hasannyesil97@gmail.com>
2020-07-13 17:23:42 +0300

    Ui changes

--- app/src/main/java/com/greenrock/getorder/ui/activity/OrderActivity.java
+++ app/src/main/java/com/greenrock/getorder/ui/activity/OrderActivity.java
@@ -1,14 +1,19 @@
 package com.greenrock.getorder.ui.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.EditText;
 import android.widget.SearchView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -32,8 +37,9 @@ public class OrderActivity extends AppCompatActivity {
     ValueEventListener orderCheckEventListener;
 
     private Toolbar mToolbar;
+    private EditText mSearcEditText;
     private RecyclerView mOrderRecyclerView;
-    private android.widget.SearchView mSearch;
+    private OrderListAdapter mOrderListAdapter;
 
     private OrderListAdapter mAdapter;
     private HashMap<String, String> mCheckOrderList;
@@ -45,10 +51,12 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
 
         mCheckOrderList = new HashMap<>();
+        mOrderListAdapter = new OrderListAdapter();
 
         mTableName = getIntent().getStringExtra("table_name");
         mProductList = getIntent().getStringArrayListExtra("product_list");
-
+        Log.d(TAG, "onCreate: table name: " + mTableName);
+        Log.d(TAG, "onCreate: product list: " + mProductList);
         initFirebase();
         initComponents();
     }
@@ -62,12 +70,14 @@ public class OrderActivity extends AppCompatActivity {
                 for (DataSnapshot child : dataSnapshot.getChildren()){
                     mCheckOrderList.put(child.getKey(),child.child("adet").getValue().toString());
                 }
+                mOrderListAdapter.setProductList(mCheckOrderList);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.d(TAG, "onCancelled: Something goes wrong, check your network connection");
             }
+
         };
 
         mCheckData.addValueEventListener(orderCheckEventListener);
@@ -76,12 +86,36 @@ public class OrderActivity extends AppCompatActivity {
     public void initComponents(){
         mToolbar = (Toolbar) findViewById(R.id.order_toolbar);
         mToolbar.setTitle(mTableName.toUpperCase());
-        mSearch = (SearchView) findViewById(R.id.product_search);
+        mSearcEditText = (EditText) findViewById(R.id.product_search);
+        mSearcEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    Intent intent = new Intent(OrderActivity.this,SearchActivity.class);
+                    intent.putExtra("products",mProductList);
+                    startActivityForResult(intent,1);
+                }
+                return false;
+            }
+        });
+        mOrderRecyclerView = (RecyclerView) findViewById(R.id.orderRecyclerView);
+        mOrderRecyclerView.setAdapter(mOrderListAdapter);
+        mOrderRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     protected void onStop() {
         mCheckData.removeEventListener(orderCheckEventListener);
         super.onStop();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            String [] product = data.getStringArrayExtra("products");
+
+        }
     }
 }
