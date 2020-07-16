0f8bb76fdc57f2e44e7e4b6353dd6a550db5d0d1
Hasan Yesil <hasannyesil97@gmail.com>
2020-07-16 19:23:18 +0300

    Firebase write function

--- app/src/main/java/com/greenrock/getorder/ui/activity/OrderActivity.java
+++ app/src/main/java/com/greenrock/getorder/ui/activity/OrderActivity.java
@@ -16,18 +16,22 @@ import android.widget.Adapter;
 import android.widget.EditText;
 import android.widget.SearchView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.greenrock.getorder.R;
+import com.greenrock.getorder.interfaces.ProductCountListener;
 import com.greenrock.getorder.ui.adapter.OrderListAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements ProductCountListener {
 
     private String TAG = "OrderTest";
     private String mTableName;
@@ -41,9 +45,8 @@ public class OrderActivity extends AppCompatActivity {
     private RecyclerView mOrderRecyclerView;
     private OrderListAdapter mOrderListAdapter;
 
-    private OrderListAdapter mAdapter;
-    private HashMap<String, String> mCheckOrderList;
-    private ArrayList<String> mProductList;
+    private HashMap<String, String> mCheckOrderList;   //
+    private HashMap<String,Float> mProductList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +57,7 @@ public class OrderActivity extends AppCompatActivity {
         mOrderListAdapter = new OrderListAdapter();
 
         mTableName = getIntent().getStringExtra("table_name");
-        mProductList = getIntent().getStringArrayListExtra("product_list");
+        mProductList = (HashMap<String, Float>) getIntent().getSerializableExtra("product_list");
         Log.d(TAG, "onCreate: table name: " + mTableName);
         Log.d(TAG, "onCreate: product list: " + mProductList);
         initFirebase();
@@ -71,6 +74,7 @@ public class OrderActivity extends AppCompatActivity {
                     mCheckOrderList.put(child.getKey(),child.child("adet").getValue().toString());
                 }
                 mOrderListAdapter.setProductList(mCheckOrderList);
+                Log.d(TAG, "onDataChange: sa");
             }
 
             @Override
@@ -91,8 +95,9 @@ public class OrderActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    ArrayList<String> productNames = new ArrayList<>(mProductList.keySet());
                     Intent intent = new Intent(OrderActivity.this,SearchActivity.class);
-                    intent.putExtra("products",mProductList);
+                    intent.putExtra("products",productNames);
                     startActivityForResult(intent,1);
                 }
                 return false;
@@ -102,6 +107,7 @@ public class OrderActivity extends AppCompatActivity {
         mOrderRecyclerView.setAdapter(mOrderListAdapter);
         mOrderRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 
     @Override
     protected void onStop() {
@@ -114,8 +120,32 @@ public class OrderActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 1 && resultCode == RESULT_OK){
-            String [] product = data.getStringArrayExtra("products");
-
+            HashMap<String,Integer> selectedProducts = (HashMap<String,Integer>) data.getSerializableExtra("selectedProducts");
+            writeFirebase(selectedProducts);
         }
+    }
+
+    public void writeFirebase(HashMap<String,Integer> productMap){
+        for (Map.Entry<String,Integer> entry : productMap.entrySet()){
+            if (mCheckOrderList.containsKey(entry.getKey())){
+                int count = Integer.parseInt(mCheckOrderList.get(entry.getKey()));
+                entry.setValue(entry.getValue() + count);
+                Log.d(TAG, "writeFirebase: Urun -> " + entry.getKey() + " Adet: " + entry.getValue());
+            }
+            mCheckData.child(entry.getKey()).child("adet").setValue(entry.getValue());
+            mCheckData.child(entry.getKey()).child("adet fiyat").setValue(mProductList.get(entry.getKey()));
+            mCheckData.removeEventListener(orderCheckEventListener);
+            mCheckData.addValueEventListener(orderCheckEventListener);
+        }
+    }
+
+    @Override
+    public void onDecreaseListener(String key, int count) {
+
+    }
+
+    @Override
+    public void onIncreaseListener(String key, int count) {
+
     }
 }
