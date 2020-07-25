59e30ccd32acdade4c3d8c494ae74d797f72895b
Hasan Yesil <hasannyesil97@gmail.com>
2020-07-25 15:10:09 +0300

    Code cleanup

--- app/src/main/java/com/greenrock/getorder/ui/adapter/ProductsListAdapter.java
+++ app/src/main/java/com/greenrock/getorder/ui/adapter/ProductsListAdapter.java
@@ -1,11 +1,9 @@
 package com.greenrock.getorder.ui.adapter;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
