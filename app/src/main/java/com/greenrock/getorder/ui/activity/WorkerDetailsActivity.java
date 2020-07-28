8b5f32c862a79d8b99cda7a81f0fd18bf2094f05
Hasan Yesil <hasannyesil97@gmail.com>
2020-07-28 20:23:06 +0300

    Ui update.

--- app/src/main/java/com/greenrock/getorder/ui/activity/WorkerDetailsActivity.java
+++ app/src/main/java/com/greenrock/getorder/ui/activity/WorkerDetailsActivity.java
@@ -31,6 +31,7 @@ public class WorkerDetailsActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private EditText mWorkerNameEdt;
+    private EditText mWorkerSurnameEdt;
     private EditText mWorkerPhoneEdt;
     private EditText mWorkerPositionEdt;
     private Button mSaveButton;
@@ -72,20 +73,23 @@ public class WorkerDetailsActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.worker_detail_toolbar);
         setSupportActionBar(mToolbar);
         mWorkerNameEdt = (EditText) findViewById(R.id.worker_name_edt);
-        mWorkerNameEdt.setText(mWorker.isim);
+        mWorkerNameEdt.setText(mWorker.isim.substring(0,1).toUpperCase() + mWorker.isim.substring(1).toLowerCase());
+        mWorkerSurnameEdt = (EditText) findViewById(R.id.worker_surname_edt);
+        mWorkerSurnameEdt.setText(mWorker.soyisim.substring(0,1).toUpperCase() + mWorker.soyisim.substring(1).toLowerCase());
         mWorkerPhoneEdt = (EditText) findViewById(R.id.worker_phone_edt);
         mWorkerPhoneEdt.setText(mWorker.telefon);
         mWorkerPositionEdt = (EditText) findViewById(R.id.worker_position_edt);
-        mWorkerPositionEdt.setText(mWorker.pozisyon);
+        mWorkerPositionEdt.setText(mWorker.pozisyon.substring(0,1).toUpperCase() + mWorker.pozisyon.substring(1).toLowerCase());
 
         mSaveButton = (Button) findViewById(R.id.saveButton);
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String oldPhoneNumber = mWorker.telefon;
-                mWorker.isim = mWorkerNameEdt.getText().toString();
+                mWorker.isim = mWorkerNameEdt.getText().toString().substring(0,1).toUpperCase() + mWorkerNameEdt.getText().toString().substring(1).trim().toLowerCase() ;
+                mWorker.soyisim = mWorkerSurnameEdt.getText().toString().substring(0,1).toUpperCase() + mWorkerSurnameEdt.getText().toString().substring(1).trim().toLowerCase();
                 mWorker.telefon = mWorkerPhoneEdt.getText().toString();
-                mWorker.pozisyon = mWorkerPositionEdt.getText().toString();
+                mWorker.pozisyon = mWorkerPositionEdt.getText().toString().toLowerCase().trim();
                 mWorkerDbRef.child(oldPhoneNumber).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
